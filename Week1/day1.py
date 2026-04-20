5e8ceeddbf6772c719afc50a9b1890cde5d997de
strwbrssnk <163606031+lapidsherwinmae@users.noreply.github.com>
2026-04-20 21:14:48 +0800

    second commit

---
+++ Week1/day1.py
@@ -0,0 +1,28 @@
+# Day 1 printing in python
+
+#In this section, I declare my name, favourite food and goal
+print("My name is Sherwin Mae Lapid")
+print("I don't have specific favourite when it comes to food")
+print("My goal for today is to finish the tasks that is given to me") 
+        
+# In this section, I declare that 5 is bigger than 2 and it's important to know that the indention matters at this part or else it will become error
+if 5 > 2:
+    print("five is greater than two")
+
+#I tried printing multiple statements but this time, I'm using semi colon while keeping it on the same line
+print("Hello World"); print("Have a good day!"); print("Python is fun")
+
+#In this section, I declare two statements in one line by just using the end="" syntax
+print("Hello world", end=" ")
+print("I will print on the same line")
+
+#In here, I print numbers without the need of the double quotation and it works fine
+print(16)
+print(23)
+print(24)
+
+#In here, I just tried to add two large numbers and I get the total of it
+print(123456789 + 2468)
+
+#In here, I just applied the things I learned in printing a statement and number in just one.
+print("I am Sherwin Mae", 22, "years old")
