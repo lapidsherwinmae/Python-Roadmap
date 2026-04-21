c09ae2efadeb28b12daed5232fb0acbdf87dd6cf
strwbrssnk <163606031+lapidsherwinmae@users.noreply.github.com>
2026-04-21 22:37:59 +0800

    third commit

---
+++ Week1/day2.py
@@ -0,0 +1,30 @@
+#I type the variables player_name and player_level that has a values of Aris as name and 5 as level.
+#After that, I print it using the print function to execute the values using variables.
+player_name = "Aris"
+player_level = 5
+print(player_name) 
+print(player_level)
+
+#I declare an age using the data type integer (int, (22))
+age = 22
+print(age)
+
+#In this statement, I delare the number with decimal using the data type float (12.5)
+inch = 12.5
+print(inch)
+
+#In this statement, I declare my name using the data type sting (str).
+name = "Sherwin Mae Lapid"
+print(name)
+
+#In this statement, I declare the variable name game_over with the value of False using the boolean (bool) data type.  
+game_over = False
+game_win = True
+print(game_over)
+print(game_win)
+
+#Type Casting
+#I converted the string number into integers.
+string_num = "15"
+convert_to_int = int(string_num)
+print(convert_to_int)
